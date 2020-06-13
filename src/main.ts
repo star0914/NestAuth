d04fe56b670d223958dd4001b97a2e325df36e6f
star0914 <58557458+star0914@users.noreply.github.com>
2020-06-13 15:57:27 +0300

    Insert Swagger doc.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('My Forum API')
+    .setDescription('My forum api description')
+    .setVersion('1.0')
+    .addTag('forum')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+  
+  await app.listen(2000);
 }
 bootstrap();
