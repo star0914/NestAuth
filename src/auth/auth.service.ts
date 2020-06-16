375f8ee341de50ca4af53f5bf69e0eb3899679bd
star0914 <58557458+star0914@users.noreply.github.com>
2020-06-16 04:30:35 +0300

    jwt configuration setting ---

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {
   }
 
   async ValidateUser(username: string, pass: string): Promise<any> { // TODO change password using bcript
@@ -14,4 +18,12 @@ export class AuthService {
     }
     return null;
   }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId};
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
+
